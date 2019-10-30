4422403cae5e8abfbc2da363ee5880e050e7cffb
James Bailey <jamesbailey2485@gmail.com>
2019-10-30 08:24:05 -0400

    initial push

--- Hello NMC/Program.cs
+++ Hello NMC/Program.cs
@@ -10,7 +10,7 @@ namespace Hello_NMC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello NMC");
+            Console.WriteLine("Hello NMC!");
         }
     }
 }
